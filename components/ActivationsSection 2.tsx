2bded6f36b3c70d390a289d51a9562e496e71eaf
M3ruf <marufuddin0@gmail.com>
2026-02-23 13:36:38 -0500

    feat: added activation section

--- components/ActivationsSection 2.tsx
+++ components/ActivationsSection 2.tsx
@@ -108,7 +108,12 @@ export default function ActivationsSection() {
         </h2>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+      <div
+        className="
+          grid gap-5 sm:gap-6
+          grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
+        "
+      >
         {ACTIVATIONS.map((activation, index) => (
           <ActivationCard
             key={activation.category}
